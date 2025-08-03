bd85eeaaf6299bd8ac7e17b4f36f6832b8d92884
lurkydismal <lurkydismal@duck.com>
2025-08-03 10:21:52 +0000

    restore default delete behavior and fix LspInlayHint highlight override
    - Reset `d`, `c`, and `x` keymaps to use unnamed register (Vim default)
    - Updated `LspInlayHint` colors in `colorscheme.lua`
    - Added `vim.schedule()` override to force `LspInlayHint` highlight to apply correctly after theme loads
    - Updated `clangd_extensions.nvim` in lazy-lock.json

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Restore default Vim behavior: delete into unnamed register
+vim.keymap.set({ "n", "x" }, "d", "d", { desc = "Delete (unnamed register)" })
+vim.keymap.set({ "n", "x" }, "c", "c", { desc = "Change (unnamed register)" })
+vim.keymap.set({ "n", "x" }, "x", "x", { desc = "Delete char (unnamed register)" })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,8 +3,8 @@ return {
     opts = {
         on_highlights = function(hl, colors)
             hl.LspInlayHint = {
-                fg = "#545c7e",
-                bg = "#24283c",
+                fg = "#dadada",
+                bg = "#003f5f",
             }
             hl.LineNrAbove = {
                 fg = "#ffffff",
@@ -20,6 +20,12 @@ return {
                 fg = vim.api.nvim_get_hl(0, { name = "Variable" }).fg,
                 italic = true,
             }
+            vim.schedule(function()
+                vim.api.nvim_set_hl(0, "LspInlayHint", {
+                    fg = "#545c7e",
+                    bg = "#24283c",
+                })
+            end)
         end,
     },
 }
