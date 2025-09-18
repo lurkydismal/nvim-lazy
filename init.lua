9c6c6a81afbc08f39d0288930857359770108a74
lurkydismal <lurkydismal@duck.com>
2025-09-18 09:23:11 +0500

    Pseudo fix biome

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-require("lspconfig.configs").biome.manager.config.single_file_support = true
+-- FIX: Update to work with new version
+-- require("lspconfig.configs").biome.manager.config.single_file_support = true
